1efb51cd8d6f8cfd134b5aeb1e1e6f94d27f10d3
A.PRANAV <pranav.a2025@vitstudent.ac.in>
2025-12-15 15:10:01 +0530

    added room code

--- client/client.py
+++ client/client.py
@@ -7,7 +7,6 @@ import time
 DISCOVERY_PORT = 50001
 TCP_PORT = 50000
 
-
 def discover_rooms(timeout=1.5):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
@@ -27,12 +26,15 @@ def discover_rooms(timeout=1.5):
 
     return found
 
+
 class Client:
     def __init__(self):
         self.sock = None
         self.players = []
         self.running = True
         self.status = "Searching for rooms..."
+        self.room_code = "" 
+        
 
     def connect(self, host):
         self.status = "Connecting..."
@@ -54,6 +56,8 @@ class Client:
                     msg = json.loads(line.decode())
                     if msg["type"] == "welcome":
                         self.id = msg["id"]
+                        if "room_code" in msg:              # <<< ROOM CODE
+                            self.room_code = msg["room_code"]
                     elif msg["type"] == "state":
                         self.players = msg["players"]
             except:
@@ -72,18 +76,19 @@ pygame.init()
 screen = pygame.display.set_mode((1000, 700))
 clock = pygame.time.Clock()
 font = pygame.font.SysFont(None, 32)
-
-
+instruction_text = font.render("Use Arrow Keys to Move", True, (255, 255, 255))
 
 client = Client()
-print("Searching for rooms...")
+print("Searching for rooms.../n")
 
 rooms = discover_rooms()
 if rooms:
-    print("Found rooms:", rooms)
-    client.connect(rooms[0]["host"])   
+    print("Found rooms:/n", rooms)
+    if "room_code" in rooms[0]:                      
+        client.room_code = rooms[0]["room_code"] 
+    client.connect(rooms[0]["host"])
 else:
-    print("No rooms found.")
+    print("No rooms found.\n")
     client.status = "No rooms found."
 
 running = True
@@ -101,21 +106,24 @@ while running:
 
     client.send_input(dx, dy)
 
-    
     screen.fill((30, 30, 30))
     for p in client.players:
         pygame.draw.rect(screen, (0,255,0), (p["x"], p["y"], 40, 40))
-    
+
     status_surface = font.render(client.status, True, (255, 255, 0))
     screen.blit(status_surface, (15, 50))
+    room_surface = font.render(f"Room: {client.room_code}", True, (0, 200, 255))  
+    text_width = room_surface.get_width()                                         
+    screen_width = screen.get_width()                                            
+    screen.blit(room_surface, (screen_width - text_width - 15, 15)) 
+    fps = clock.get_fps()  
+    fps_text = font.render(f"FPS: {int(fps)}", True, (255, 255, 255))
+    screen.blit(fps_text, (15, 80))
 
-
+    screen.blit(instruction_text, (15, 120))
 
     pygame.display.flip()
     clock.tick(60)
 
 pygame.quit()
 client.running = False
-
-    
-
